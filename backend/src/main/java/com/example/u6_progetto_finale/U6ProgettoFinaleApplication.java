c9e15033904035741208e6f2cb7c96635f005cdc
simone pierantozzi <pierantozzisimone@gmail.com>
2026-09-10 11:40:13 +0200

    primo commit

---
+++ backend/src/main/java/com/example/u6_progetto_finale/U6ProgettoFinaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.u6_progetto_finale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U6ProgettoFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U6ProgettoFinaleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/u6_progetto_finale/U6ProgettoFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.u6_progetto_finale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6ProgettoFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
